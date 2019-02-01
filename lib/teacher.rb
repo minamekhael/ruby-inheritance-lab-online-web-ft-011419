1e2f6619a270fb31f285eab93d1f27f7bd55f33a
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-01 02:47:34 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -2,10 +2,4 @@ class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
-    def teach 
-    @knowledge.split(",")
-    @knowledge.each do |value|
-      puts value
-    end
-  end
 end
